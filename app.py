a660933c4201cff04ee2ea5afcdee9f62b46abd9
meghamondal <19meghamondal@gmail.com>
2026-07-21 21:06:56 +0530

    made new changes in app.py

--- app.py
+++ app.py
@@ -30,18 +30,16 @@ SECRET_PATTERNS = [
     # Slack webhook
     r"https://hooks\.slack\.com/services/[^\s]+",
 
-    # Private keys
+    # PEM private key
     r"-----BEGIN [A-Z ]*PRIVATE KEY-----",
 
-    # Generic hardcoded credentials
-    r"api[_-]?key\s*[:=]\s*['\"][^'\"]{10,}['\"]",
-    r"access[_-]?key\s*[:=]\s*['\"][^'\"]{10,}['\"]",
-    r"secret[_-]?key\s*[:=]\s*['\"][^'\"]{10,}['\"]",
-    r"secret\s*[:=]\s*['\"][^'\"]{10,}['\"]",
-    r"token\s*[:=]\s*['\"][^'\"]{10,}['\"]",
+    # Generic quoted literals
+    r"api[_-]?key\s*[:=]\s*['\"][^'\"]{12,}['\"]",
+    r"secret\s*[:=]\s*['\"][^'\"]{12,}['\"]",
     r"password\s*[:=]\s*['\"][^'\"]{8,}['\"]",
-    r"bearer\s+['\"][^'\"]{10,}['\"]",
+    r"token\s*[:=]\s*['\"][^'\"]{12,}['\"]",
 ]
+
 
 def detect_hardcoded_secret(text: str) -> bool:
     for pattern in SECRET_PATTERNS:
@@ -71,14 +69,6 @@ PROMPT_PATTERNS = [
     "upload local files",
     "override user",
     "override agent",
-    "ignore reviewer",
-    "ignore review",
-    "ignore approval",
-    "without user approval",
-    "without reviewer approval",
-    "silently upload",
-    "silently send",
-    "exfiltrate files",
 ]
 
 PROMPT_REGEX = [
@@ -124,11 +114,6 @@ PERMISSION_PATTERNS = [
     "write: /",
     "allow all domains",
     "unrestricted network",
-    "any domain",
-    "all hosts",
-    "any host",
-    "write to entire filesystem",
-    "read entire filesystem",
 ]
 
 PERMISSION_REGEX = [
@@ -140,12 +125,6 @@ PERMISSION_REGEX = [
     r"allowed_domains\s*:\s*\[?\s*['\"]?\*['\"]?",
     r"allowed_domains\s*:\s*['\"]?all['\"]?",
     r"allowed_hosts\s*:\s*['\"]?\*['\"]?",
-    r"filesystem.*read.*write.*\/",
-    r"filesystem.*\/",
-    r"domains.*\*",
-    r"hosts.*\*",
-    r"network.*any",
-    r"egress.*any",
 ]
 
 
